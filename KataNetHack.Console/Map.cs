3be0a4e01d6f10747280822dab5bf3715d548885
d-burriss <d.burriss@coolblue.nl>
2016-09-09 17:26:09 +0200

    basic creation of map

---
+++ KataNetHack.Console/Map.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace KataNetHack.Console
+{
+    public class Map
+    {
+
+        public IEnumerable<Element> Elements { get; set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Elements = new List<Element>();
+        }
+
+        public static Map Create(int width, int height)
+        {
+            var map = new Map(width, height);
+            map.Elements = PopulateElements();
+            return map;
+        }
+
+        private static IEnumerable<Element> PopulateElements(int number = 100)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                yield return new Element();
+            }
+        }
+
+    }
+
+    public class Element
+    {
+        //type, x, y
+    }
+}
---
+++ KataNetHack.Tests/MapTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using KataNetHack.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataNetHack.Tests
+{
+    [TestClass]
+    public class MapTests
+    {
+        [TestMethod]
+        public void Create_OnMapFactory_ReturnsMap()
+        {
+            var sut = Map.Create(0, 0);
+            Assert.IsInstanceOfType(sut, typeof(Map));
+        }
+
+        [TestMethod]
+        public void Elements_OnMap_Returns100Elements()
+        {
+            var sut = Map.Create(10, 10);
+            var elements = sut.Elements;
+            Assert.AreEqual(100, elements.Count());
+        }
+
+        [TestMethod]
+        public void Width_OnMapWith10Width_Returns10()
+        {
+            var sut = Map.Create(10, 0);
+            Assert.AreEqual(10, sut.Width);
+        }
+
+        [TestMethod]
+        public void Height_OnMapWith10Width_Returns10()
+        {
+            var sut = Map.Create(0, 10);
+            Assert.AreEqual(10, sut.Height);
+        }
+
+    }
+}
