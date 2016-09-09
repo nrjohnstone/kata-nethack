f963ce4894a961e587e53ce4e1920033314442e9
Erik Heemskerk <e.heemskerk@coolblue.nl>
2016-09-09 17:16:14 +0200

    Moved Input and InputResult classes to Console project

--- KataNetHack.Tests/InputTests.cs
+++ KataNetHack.Tests/InputTests.cs
@@ -2,6 +2,8 @@
 
 using FluentAssertions;
 
+using KataNetHack.Console.Input;
+
 using Xunit;
 
 namespace KataNetHack.Tests
@@ -42,40 +44,5 @@ namespace KataNetHack.Tests
         {
             return new ConsoleKeyInfo('Q', ConsoleKey.Q, false, false, false );
         }
-    }
-
-    public class Input
-    {
-        public Func<ConsoleKeyInfo> ReadKey = () => System.Console.ReadKey();
-
-        public InputResult GetInput()
-        {
-            var key = ReadKey();
-
-            switch(key.Key)
-            {
-                case ConsoleKey.W:
-                    return InputResult.Up;
-                case ConsoleKey.A:
-                    return InputResult.Left;
-                case ConsoleKey.S:
-                    return InputResult.Down;
-                case ConsoleKey.D:
-                    return InputResult.Right;
-                default:
-                    return InputResult.Invalid;
-            }
-        }
-    }
-
-
-
-    public enum InputResult
-    {
-        Invalid,
-        Up,
-        Down,
-        Left,
-        Right
     }
 }
