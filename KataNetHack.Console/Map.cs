ffe035334207f7df3134809b7c01a030fcca8f27
d-burriss <d.burriss@coolblue.nl>
2016-09-30 17:04:33 +0200

    Implemented IMap

--- KataNetHack.Console/Map.cs
+++ KataNetHack.Console/Map.cs
@@ -3,7 +3,14 @@ using System.Linq;
 
 namespace KataNetHack.Console
 {
-    public class Map
+    public interface IMap
+    {
+        bool CanMoveTo(int x, int y);
+        bool IsExit(int x, int y);
+        ElementType GetElementType(int x, int y);
+    }
+
+    public class Map : IMap
     {
 
         public IEnumerable<Element> Elements { get; set; }
@@ -26,17 +33,54 @@ namespace KataNetHack.Console
 
         private static IEnumerable<Element> PopulateElements(int number = 100)
         {
+            
             for (int i = 0; i < number; i++)
             {
-                yield return new Element();
+                if (i < 10)
+                {
+                    yield return new Element();
+                }
+                else if (i == 99)
+                {
+                    yield return new Element() { Type = ElementType.Exit };
+                }
+                else
+                {
+                    yield return new Element() { Type = ElementType.PassageWay };
+                }
             }
         }
 
         public Element ElementAt(int x, int y)
         {
-            return Elements.First();
+            int position = (y * Width) - x + 1;
+            if (y == 10)
+            {
+                position = (y * Width - 1);
+                return Elements.ToArray()[position];
+            }
+            
+            return Elements.ToArray()[position];
+        }
+
+        public bool CanMoveTo(int x, int y)
+        {
+            return ElementAt(x, y).Type != ElementType.Wall;
+        }
+
+        public bool IsExit(int x, int y)
+        {
+            var element = ElementAt(x, y);
+            return element.Type == ElementType.Exit;
+        }
+
+        public ElementType GetElementType(int x, int y)
+        {
+            return ElementAt(x, y).Type;
         }
     }
+
+    
 
     public class Element
     {
--- KataNetHack.Tests/MapTests.cs
+++ KataNetHack.Tests/MapTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using FluentAssertions;
 using KataNetHack.Console;
 using Xunit;
 
@@ -30,7 +31,7 @@ namespace KataNetHack.Tests
         }
 
         [Fact]
-        public void Height_OnMapWith10Width_Returns10()
+        public void Height_OnMapWith10Height_Returns10()
         {
             var sut = Map.Create(0, 10);
             Assert.Equal(10, sut.Height);
@@ -44,5 +45,111 @@ namespace KataNetHack.Tests
             Assert.Equal(corner.Type, ElementType.Wall);
         }
 
+        [Fact]
+        public void CanMoveTo_AWallOnA10X10Map_ShouldReturnFalse_()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.CanMoveTo(0, 0);
+
+            result.Should().BeFalse();
+        }
+
+
+        [Fact]
+        public void CanMoveTo_OverAValidCoordinateOnA10X10Map_ShouldReturnTrue()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.CanMoveTo(1, 1);
+
+            result.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void CanMoveTo_OverAnotherValidCoordinateOnA10X10Map_ShouldReturnTrue()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.CanMoveTo(4, 5);
+
+            result.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void CanMoveTo_OverYetAnotherValidCoordinateOnA10X10Map_ShouldReturnTrue()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.CanMoveTo(9, 7);
+
+            result.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void CanMoveTo_OverExitOnA10X10Map_ShouldReturnTrue()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.CanMoveTo(10, 10);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsExit_OnAWall_IsFalse()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.IsExit(0, 0);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsExit_OnAPassageWay_IsFalse()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.IsExit(1, 1);
+
+            result.Should().BeFalse();
+        }
+
+
+        [Fact]
+        public void IsExit_OnAnExit_IsTrue()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.IsExit(10, 10);
+
+            result.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void GetElementType_OnAWall_ReturnsWall()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.GetElementType(0, 0);
+
+            result.Should().Be(ElementType.Wall);
+        }
+
+
+        [Fact]
+        public void GetElementType_OnAPassageWay_ReturnsPassageWay()
+        {
+            var sut = Map.Create(10, 10);
+
+            var result = sut.GetElementType(5, 5);
+
+            result.Should().Be(ElementType.PassageWay);
+        }
     }
 }
