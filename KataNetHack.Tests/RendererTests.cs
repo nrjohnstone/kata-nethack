6647a332feebcd4ccbaa7fff71c7106671d791b5
Erik Heemskerk <e.heemskerk@coolblue.nl>
2016-10-28 16:36:15 +0200

    Add Renderer which can render the map

--- KataNetHack.Tests/RendererTests.cs
+++ KataNetHack.Tests/RendererTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-using FluentAssertions;
+﻿using FluentAssertions;
 
 using KataNetHack.Console;
-
-using Moq;
 
 using Xunit;
 
@@ -56,98 +50,25 @@ namespace KataNetHack.Tests
             builder.AssertRowEmpty(1);
         }
 
-        [Fact]
-        public void Render_MismatchingXYCoordinatesWall_RendersCellAsWall()
+        [Theory]
+        [InlineData(ElementType.Wall, '█')]
+        [InlineData(ElementType.Exit, '◌')]
+        public void Render_MismatchingXYCoordinates_RendersCellAsExpected(ElementType elementType, char expected)
         {
             var builder = new RendererBuilder();
 
-            var sut = builder.WithWallAt(3, 2)
+            var sut = builder.WithCellAt(3, 2, elementType)
                              .Build();
 
             sut.Render();
 
+            string leftPadding = "   ";
+            string rightPadding = "      ";
+
             builder.AssertRowEmpty(0);
             builder.AssertRowEmpty(1);
-            builder.OutputLines[2].Should().Be("   █      ");
+            builder.OutputLines[2].Should().Be(leftPadding + expected + rightPadding);
             builder.AssertRowEmpty(3);
-        }
-    }
-
-    public class Renderer
-    {
-        private const int MAP_ROW_COUNT = 10;
-        private const int MAP_COLUMN_COUNT = 10;
-        private const char WALL_CHARACTER = '█';
-        private const char PASSAGEWAY_CHARACTER = ' ';
-
-        private readonly IMap _map;
-
-        public Action<string> WriteLine = System.Console.WriteLine;
-
-        public Renderer(IMap map)
-        {
-            _map = map;
-        }
-
-        public void Render()
-        {
-            for(int row = 0;row < MAP_ROW_COUNT;row++)
-            {
-                var builder = new StringBuilder();
-
-                for(var column = 0;column < MAP_COLUMN_COUNT;column++)
-                {
-                    if(_map.GetElementType(column, row) == ElementType.Wall)
-                    {
-                        builder.Append(WALL_CHARACTER);
-                    }
-                    else
-                    {
-                        builder.Append(PASSAGEWAY_CHARACTER);
-                    }
-                }
-
-                WriteLine(builder.ToString());
-            }
-        }
-    }
-
-    public class RendererBuilder
-    {
-        private Mock<IMap> Map { get; } = new Mock<IMap>();
-        public IReadOnlyList<string> OutputLines => _lines;
-        private readonly List<string> _lines = new List<string>();
-
-        public RendererBuilder()
-        {
-            Map.Setup(obj => obj.GetElementType(It.IsAny<int>(), It.IsAny<int>()))
-               .Returns(ElementType.PassageWay);
-        }
-
-        public RendererBuilder WithWallAt(int x, int y)
-        {
-            Map.Setup(obj => obj.GetElementType(x, y))
-               .Returns(ElementType.Wall);
-
-            return this;
-        }
-
-        public Renderer Build()
-        {
-            return new Renderer(Map.Object)
-                   {
-                       WriteLine = line => _lines.Add(line)
-                   };
-        }
-    }
-
-    public static class RendererAssertions
-    {
-        private static readonly string EMPTY_ROW = new string(' ', 10);
-
-        public static void AssertRowEmpty(this RendererBuilder builder, int row)
-        {
-            builder.OutputLines[row].Should().Be(EMPTY_ROW);
         }
     }
 }
