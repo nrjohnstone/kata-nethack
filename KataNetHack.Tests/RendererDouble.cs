c84dcaf06be21f559bc7d65712163b2e328a113d
sandermvanvliet <sander@codenizer.nl>
2016-10-28 18:39:05 +0200

    Renders the player

---
+++ KataNetHack.Tests/RendererDouble.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using KataNetHack.Console;
+using KataNetHack.Console.Renderer;
+
+namespace KataNetHack.Tests
+{
+    public class RendererDouble : Renderer
+    {
+        public List<string> Output { get; }
+
+        public RendererDouble(IMap map) : base(map)
+        {
+            Output = new List<string>();
+            WriteLine = line => Output.Add(line);
+            ClearScreen = () => Output.Clear();
+        }
+    }
+}
---
+++ KataNetHack.Tests/WhenRenderingTheGame.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using KataNetHack.Console.Input;
+using KataNetHack.Console.Maps;
+using KataNetHack.Console.PlayerSubsystem;
+using KataNetHack.Console.Renderer;
+using Xunit;
+
+namespace KataNetHack.Tests
+{
+    public class WhenRenderingTheGame
+    {
+        [Fact]
+        public void ThenItRendersThePlayer()
+        {
+            var map = new Stage1().LoadMap();
+            var input = new InputDouble();
+            var renderer = new RendererDouble(map);
+
+            var engine = new GameEngineBuilder()
+                .WithInput(input)
+                .WithRenderer(renderer)
+                .WithPlayer(new Player(5, 5))
+                .Build();
+
+            input.SendInput(InputResult.East);
+
+            renderer
+                .Output[4]
+                .Should()
+                .Contain(RenderableFactory.PLAYER_REPRESENTATION.ToString());
+        }
+    }
+}
